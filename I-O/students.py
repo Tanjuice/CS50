bc89c797cea2f21895ca62e28210c5d549a1935b
Tanjuice <138823364+Tanjuice@users.noreply.github.com>
2023-07-17 00:59:10 +0100

    2

--- I-O/students.py
+++ I-O/students.py
@@ -95,14 +95,34 @@
 
 #-------------------
 
+# import csv
+
+# students = []
+    
+# with open("students.csv") as file:
+#   reader = csv.DictReader(file)
+#   for row in reader:
+#      students.append({"name": row["name"], "home": row["home"]})
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#    print(f"{student['name']} is from {student['home']}")
+#---------------
+
+# import csv
+
+# name = input("What's your name? ")
+# home = input("Where's your home? ")
+
+# with open("students.csv", "a") as file:
+#     writer = csv.writer(file)
+#     writer.writerow([name, home])
+#--------------
+
 import csv
 
-students = []
-    
-with open("students.csv") as file:
-  reader = csv.DictReader(file)
-  for row in reader:
-     students.append({"name": row["name"], "home": row["home"]})
+name = input("What's your name? ")
+home = input("Where's your home? ")
 
-for student in sorted(students, key=lambda student: student["name"]):
-   print(f"{student['name']} is from {student['home']}")
+with open("students.csv", "a", newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
