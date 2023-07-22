9d2d9c54345a2eeb5350f17076e944eac40c6fa2
Tanjuice <138823364+Tanjuice@users.noreply.github.com>
2023-07-22 16:34:31 +0100

    oop and et cetera

---
+++ Et Cetera/meows.py
@@ -0,0 +1,11 @@
+class Cat:
+    MEOWS = 3
+
+    def meow(self):
+        for _ in range(Cat.MEOWS):
+            print("Meow")
+
+
+cat = Cat
+
+cat.meow(1)
--- OOP/student.py
+++ OOP/student.py
@@ -1,29 +1,22 @@
 class Student:
-    def __init__(self, name, house, patronus):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytheryn"]:
-            raise ValueError("Invalid House")
+    def __init__(self, name, house):
         self.name = name
         self.house = house
-        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name,house)
+
 
 def main():
-    student = get_student()
+    student = Student.get()
     print(student)
     
-
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    patronus = input("Patronus: ")
-    return Student(name, house, patronus)
-
 
 if __name__ == "__main__":
     main()
